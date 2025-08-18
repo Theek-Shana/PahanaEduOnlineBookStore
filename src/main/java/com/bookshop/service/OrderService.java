971c3b705e593aeb81a3b4b287f8b11fcd8760e9
Theek-Shana <mahesh190290390@gmail.com>
2025-08-18 20:24:10 +0530

    updated order service

--- src/main/java/com/bookshop/service/OrderService.java
+++ src/main/java/com/bookshop/service/OrderService.java
@@ -3,29 +3,69 @@ package com.bookshop.service;
 import com.bookshop.dao.OrderDAO;
 import com.bookshop.model.Order;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
 public class OrderService {
 
-    private OrderDAO orderDAO;
+    private static OrderService instance;
+    private final OrderDAO orderDAO;
 
-    public OrderService() throws SQLException {
-        this.orderDAO = new OrderDAO(); 
+    // Private constructor for Singleton
+    private OrderService() throws SQLException {
+        this.orderDAO = new OrderDAO();
     }
 
-    public boolean placeOrder(Order order) throws SQLException {
-        int orderId = orderDAO.placeOrder(order);
-        return orderId > 0;
+    // Singleton getInstance method
+    public static OrderService getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new OrderService();
+        }
+        return instance;
     }
 
-    public List<Order> getAllOrders() throws SQLException {
-        return orderDAO.getAllOrders();
+    /**
+     * Place a new order.
+     * @param order The order object
+     * @return true if order placed successfully
+     */
+    public boolean placeOrder(Order order) {
+        try {
+            int orderId = orderDAO.placeOrder(order);
+            return orderId > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
-    public boolean updateOrderStatus(int orderId, String status, String message, int staffId) throws SQLException {
-        return orderDAO.updateOrderStatus(orderId, status, message, staffId);
-    } 
+    /**
+     * Get all orders.
+     * @return List of orders
+     */
+    public List<Order> getAllOrders() {
+        try {
+            return orderDAO.getAllOrders();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return List.of(); 
+        }
+    }
+
+    /**
+     * Update order status.
+     * @param orderId ID of the order
+     * @param status New status
+     * @param message Optional message
+     * @param staffId Staff who updated
+     * @return true if update successful
+     */
+    public boolean updateOrderStatus(int orderId, String status, String message, int staffId) {
+        try {
+            return orderDAO.updateOrderStatus(orderId, status, message, staffId);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
-
