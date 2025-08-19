7559e8f9c877f6d74a0942b46cb0b5afbf9339a1
Theek-Shana <mahesh190290390@gmail.com>
2025-08-19 17:27:04 +0530

    dbConn error check

--- src/main/java/com/bookshop/dao/DBConnection.java
+++ src/main/java/com/bookshop/dao/DBConnection.java
@@ -4,32 +4,34 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
+
 public class DBConnection {
     private static DBConnection instance;
-    private Connection connection;
     private String url = "jdbc:mysql://localhost:3306/pahanaonline";
     private String username = "root";
-    private String password = "Theek@2004"; 
+    private String password = "Theek@2004";
 
     private DBConnection() throws SQLException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            this.connection = DriverManager.getConnection(url, username, password);
-        } catch (ClassNotFoundException ex) {
-            System.out.println("DB Connection creation failed: " + ex.getMessage());
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Driver not found", e);
         }
     }
 
     public Connection getConnection() throws SQLException {
+        // return a new connection each time
         return DriverManager.getConnection(url, username, password);
     }
 
-    public static DBConnection getInstance() throws SQLException {
-        if (instance == null || instance.getConnection().isClosed()) {
+    public static synchronized DBConnection getInstance() throws SQLException {
+        if (instance == null) {
             instance = new DBConnection();
         }
         return instance;
     }
 }
+
 
  
